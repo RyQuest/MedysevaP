141aea2a361e373db0e5b62c16a92a7664669022
Ashish Banjare <bleshbanz@gmail.com>
2022-07-04 12:05:13 +0530

    get all appointments  response modified

--- app/Http/Controllers/Api/Doctor/DoctorController.php
+++ app/Http/Controllers/Api/Doctor/DoctorController.php
@@ -13,21 +13,29 @@ class DoctorController extends Controller
     /* get all appoinments data */
     public function appointments(Request $request){
         
-        $user_id     = $request->input('user_id');
+         $user_id     = $request->input('user_id');
         $chamber_uid = $request->input('chamber_uid');
         $offset      = $request->input('offset');
         $limit       = $request->input('limit');
         
         $user = User::find($user_id);
 
-
-        $dr_query = DB::table('appointments')->select('id', 'date');
+        $dr_query = DB::table('appointments');
+        // $dr_query->leftJoin('patientses', 'patientses.id', '=', 'appointments.patient_id');
 
         if($user->chamber_id > 0)        {
             $dr_query->where('chamber_id',$chamber_uid);
         }
 
-        $dr_query->groupBy('date');
+        /*if($user->doctor_type == "0"){
+            $dr_query->where('appointment_type',"1");
+        } else if($user->doctor_type == "1"){
+            $dr_query->where('user_id', $user->id);
+        }*/
+
+        $dr_query->orderBy('date', 'ASC');
+        // $dr_query->orderBy('serial_id', 'desc');
+
         $dr_query->skip($offset);
         $dr_query->take($limit);
 
@@ -35,12 +43,28 @@ class DoctorController extends Controller
 
         foreach ($appointments as $key => $value) {
 
-            $query2 = DB::table('appointments');
+            $appointments[$key]->patient = DB::table('patientses')->where('id', $value->patient_id)->first();
+            $appointments[$key]->chamber = DB::table('chamber')->where('uid', $value->chamber_id)->first();
 
-            $query2->where('date',$value->date);
-            
-            $appointments[$key]->total_patients = $query2->count();
+            $query2 = DB::table('prescription as p');
+            $query2->where('p.patient_id', $value->patient_id);
+            $query2->where(DB::raw("(DATE_FORMAT(p.created_at,'%Y-%m-%d'))"), $value->date);
+            $appointments[$key]->is_done = $query2->count();
 
+            // User Payment status
+            $query3 = DB::table('payment_user');
+            $query3->where('user_id', $value->user_id);
+            $query3->where('appointment_id', $value->id);
+            $payment = $query3->first();
+            if (empty($payment)) {
+                $appointments[$key]->payment_status = 0;
+            } else {
+                if ($payment->status == 'verified') {
+                    $appointments[$key]->payment_status = 1;
+                } else {
+                    $appointments[$key]->payment_status = 0;
+                }
+            }
         }
 
         if(!empty($appointments)){
