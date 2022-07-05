c9d0a9b08f3418847e45a0d32420c5243e1e7f5e
Ashish Banjare <bleshbanz@gmail.com>
2022-07-05 17:12:23 +0530

    bootstrap cache commit

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -192,7 +192,7 @@
       'file' => 
       array (
         'driver' => 'file',
-        'path' => '/var/www/html/storage/framework/cache/data',
+        'path' => '/var/www/html/laravels/MedysevaP/storage/framework/cache/data',
       ),
       'memcached' => 
       array (
@@ -275,7 +275,7 @@
       array (
         'driver' => 'sqlite',
         'url' => NULL,
-        'database' => 'clinic',
+        'database' => 'medyseva',
         'prefix' => '',
         'foreign_key_constraints' => true,
       ),
@@ -283,11 +283,11 @@
       array (
         'driver' => 'mysql',
         'url' => NULL,
-        'host' => 'clinic.cjmcoyve6pck.ap-south-1.rds.amazonaws.com',
+        'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'clinic',
-        'username' => 'medyseva',
-        'password' => 'Medyseva$#123',
+        'database' => 'medyseva',
+        'username' => 'root',
+        'password' => 'root',
         'unix_socket' => '',
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
@@ -303,11 +303,11 @@
       array (
         'driver' => 'pgsql',
         'url' => NULL,
-        'host' => 'clinic.cjmcoyve6pck.ap-south-1.rds.amazonaws.com',
+        'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'clinic',
-        'username' => 'medyseva',
-        'password' => 'Medyseva$#123',
+        'database' => 'medyseva',
+        'username' => 'root',
+        'password' => 'root',
         'charset' => 'utf8',
         'prefix' => '',
         'prefix_indexes' => true,
@@ -318,11 +318,11 @@
       array (
         'driver' => 'sqlsrv',
         'url' => NULL,
-        'host' => 'clinic.cjmcoyve6pck.ap-south-1.rds.amazonaws.com',
+        'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'clinic',
-        'username' => 'medyseva',
-        'password' => 'Medyseva$#123',
+        'database' => 'medyseva',
+        'username' => 'root',
+        'password' => 'root',
         'charset' => 'utf8',
         'prefix' => '',
         'prefix_indexes' => true,
@@ -363,12 +363,12 @@
       'local' => 
       array (
         'driver' => 'local',
-        'root' => '/var/www/html/storage/app',
+        'root' => '/var/www/html/laravels/MedysevaP/storage/app',
       ),
       'public' => 
       array (
         'driver' => 'local',
-        'root' => '/var/www/html/storage/app/public',
+        'root' => '/var/www/html/laravels/MedysevaP/storage/app/public',
         'url' => 'http://127.0.0.1:8000/storage',
         'visibility' => 'public',
       ),
@@ -386,7 +386,7 @@
     ),
     'links' => 
     array (
-      '/var/www/html/public/storage' => '/var/www/html/storage/app/public',
+      '/var/www/html/laravels/MedysevaP/public/storage' => '/var/www/html/laravels/MedysevaP/storage/app/public',
     ),
   ),
   'hashing' => 
@@ -405,7 +405,7 @@
   ),
   'jwt' => 
   array (
-    'secret' => 'lokScJkLfH7DnIm9b6W4tQ5qHLg1xGPErmHdDu83DD42fn6fZlpvdCQ1yF2YHlFQ',
+    'secret' => 's0jqBrjTZXe9K6JxBOAhMDsXLNlBTxF1eo89ararMQH7ewlvhbPLlN49dbXIeS26',
     'keys' => 
     array (
       'public' => NULL,
@@ -456,13 +456,13 @@
       'single' => 
       array (
         'driver' => 'single',
-        'path' => '/var/www/html/storage/logs/laravel.log',
+        'path' => '/var/www/html/laravels/MedysevaP/storage/logs/laravel.log',
         'level' => 'debug',
       ),
       'daily' => 
       array (
         'driver' => 'daily',
-        'path' => '/var/www/html/storage/logs/laravel.log',
+        'path' => '/var/www/html/laravels/MedysevaP/storage/logs/laravel.log',
         'level' => 'debug',
         'days' => 14,
       ),
@@ -513,7 +513,7 @@
       ),
       'emergency' => 
       array (
-        'path' => '/var/www/html/storage/logs/laravel.log',
+        'path' => '/var/www/html/laravels/MedysevaP/storage/logs/laravel.log',
       ),
     ),
   ),
@@ -526,10 +526,10 @@
       array (
         'transport' => 'smtp',
         'host' => 'smtp.gmail.com',
-        'port' => '465',
-        'encryption' => 'ssl',
-        'username' => 'clubvirtual.help@gmail.com',
-        'password' => 'Mwz4>YScjV/T',
+        'port' => '587',
+        'encryption' => 'tls',
+        'username' => 'medyseva.help@gmail.com',
+        'password' => 'chanwsoljnijtlnw',
         'timeout' => NULL,
         'auth_mode' => NULL,
       ),
@@ -571,7 +571,7 @@
     ),
     'from' => 
     array (
-      'address' => 'clubvirtual.help@gmail.com',
+      'address' => 'medyseva@gmail.com',
       'name' => 'JokerNft',
     ),
     'markdown' => 
@@ -579,7 +579,7 @@
       'theme' => 'default',
       'paths' => 
       array (
-        0 => '/var/www/html/resources/views/vendor/mail',
+        0 => '/var/www/html/laravels/MedysevaP/resources/views/vendor/mail',
       ),
     ),
   ),
@@ -684,7 +684,7 @@
     'lifetime' => '120',
     'expire_on_close' => false,
     'encrypt' => false,
-    'files' => '/var/www/html/storage/framework/sessions',
+    'files' => '/var/www/html/laravels/MedysevaP/storage/framework/sessions',
     'connection' => NULL,
     'table' => 'sessions',
     'store' => NULL,
@@ -704,9 +704,9 @@
   array (
     'paths' => 
     array (
-      0 => '/var/www/html/resources/views',
+      0 => '/var/www/html/laravels/MedysevaP/resources/views',
     ),
-    'compiled' => '/var/www/html/storage/framework/views',
+    'compiled' => '/var/www/html/laravels/MedysevaP/storage/framework/views',
   ),
   'flare' => 
   array (
@@ -853,7 +853,7 @@
     ),
     'temporary_files' => 
     array (
-      'local_path' => '/var/www/html/storage/framework/cache/laravel-excel',
+      'local_path' => '/var/www/html/laravels/MedysevaP/storage/framework/cache/laravel-excel',
       'remote_disk' => NULL,
       'remote_prefix' => NULL,
       'force_resync_remote' => NULL,
